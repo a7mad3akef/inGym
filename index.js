0aa25703e6b8a9c787c1a270141f52586e1b45f0
a7mad3akef <a7mad3akef@gmail.com>
2018-02-13 16:04:25 +0200

    add list to response

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ function handleMessage(sender_psid, received_message) {
       // Create the payload for a basic text message, which
       // will be added to the body of our request to the Send API
       if (received_message.text == 'list'){
+        console.log('accepting request to display list')
         response = {
             "attachment": {
                 "type": "template",
