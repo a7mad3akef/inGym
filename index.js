9bd7ba929fd7b83a2c3c82ca8f697ad6c0792499
a7mad3akef <a7mad3akef@gmail.com>
2018-02-14 14:10:34 +0200

    fix a typo

--- index.js
+++ index.js
@@ -87,7 +87,9 @@ app.get('/webhook', (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
-    getUserInfo(sender_psid);
+    getUserInfo(sender_psid,function(){
+        
+    });
     // Checks if the message contains text
     if (received_message.text) {    
       // Create the payload for a basic text message, which
